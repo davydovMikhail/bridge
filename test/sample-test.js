c380737292b668334dc62da68f479cac538fa3c9
davydovMikhail <davydov27977@gmail.com>
2022-03-26 23:07:05 +0700

    tests2

--- test/sample-test.js
+++ test/sample-test.js
@@ -28,10 +28,15 @@ describe("Bridge", function () {
 
 
   it("main loop", async function () {
+    const otherSymbol = 'TOKIUN'
     const balance = parseEther('100');
     await tokenFrom.connect(owner).transfer(user1.address, balance);
     const id = 1
+    const otherId = 2
+    await expect(bridgeFrom.connect(user1).swap(id, balance, user2.address, chainFrom, symbol)).to.be.revertedWith("recipient's chainId was not added")
+    await expect(bridgeFrom.connect(user1).swap(id, balance, user2.address, chainTo, otherSymbol)).to.be.revertedWith("the token was not added to the bridge")
     await bridgeFrom.connect(user1).swap(id, balance, user2.address, chainTo, symbol);
+    await expect(bridgeFrom.connect(user1).swap(id, balance, user2.address, chainTo, symbol)).to.be.revertedWith("swap already completed")
     expect(await tokenFrom.balanceOf(user2.address)).to.equal(0);
     const message = hre.web3.utils.soliditySha3(
       id,
@@ -43,7 +48,11 @@ describe("Bridge", function () {
     )
     const signature = await hre.web3.eth.sign(message, validator);
     let splitedSignature = hre.ethers.utils.splitSignature(signature);
+    await expect(bridgeTo.connect(user2).redeem(id, balance, user2.address, chainFrom, chainFrom, symbol, splitedSignature.v, splitedSignature.r, splitedSignature.s)).to.be.revertedWith("Error: chainTo and chainFrom is the same")
+    await expect(bridgeTo.connect(user2).redeem(id, balance, user2.address, chainFrom, chainTo, otherSymbol, splitedSignature.v, splitedSignature.r, splitedSignature.s)).to.be.revertedWith("the token was not added to the bridge")
+    await expect(bridgeTo.connect(user2).redeem(otherId, balance, user2.address, chainFrom, chainTo, symbol, splitedSignature.v, splitedSignature.r, splitedSignature.s)).to.be.revertedWith("")
     await bridgeTo.connect(user2).redeem(id, balance, user2.address, chainFrom, chainTo, symbol, splitedSignature.v, splitedSignature.r, splitedSignature.s);
     expect(await tokenTo.balanceOf(user2.address)).to.equal(balance);
+    await bridgeFrom.connect(owner).excludeToken(symbol)
   });
 });
